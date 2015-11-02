16665df3206d182a657c903923084ebe694ab042
walid <walid.sa3d@gmail.com>
2015-11-02 18:53:03 +0100

    create a cache for shows

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-hello world
+var request = require('request-promise');
+var cheerio = require('cheerio');
+var fs = require('fs');
+
+var shows_url = 'http://www.addic7ed.com/shows.php';
+
+var cacheShows = function(){
+    var shows = [];
+    var options = {
+        uri: shows_url,
+        transform: function(body) {
+            return cheerio.load(body);
+        }
+    };
+    request(options).then(function($){
+        $('h3 > a').each(function(index, element){
+            var show = {};
+            show.name = $(element).text();
+            show.id = /\d+/.exec($(element).attr('href'))[0];
+            shows.push(show);
+        } );
+        data = JSON.stringify(shows);
+        saveData(data, 'cache.json');
+
+    });
+};
+function saveData(data, filepath){
+    fs.writeFile(filepath, data, function (err) {
+      if (err) return console.log(err);
+      console.log('Data Saved');
+    });
+
+}
+cacheShows();
+
+
+
+
